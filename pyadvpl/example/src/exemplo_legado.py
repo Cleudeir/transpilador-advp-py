8009ca64e7b20f1201026916c5759fe4ee4a3614
Cleudeir <cleudeirSilva@gmail.com>
2026-04-22 05:54:45 -0300

    refactor: remove legacy example script from project source

--- pyadvpl/example/src/exemplo_legado.py
+++
@@ -1,7 +0,0 @@
-# PREPROCESSOR: #include 'protheus.ch'
-# Exemplo de cÃ³digo legado para ser convertido para Python
-def u_ExemploLegado():
-    nI = 0
-    cMsg = 'Iniciando processamento...'
-    MsgAlert(cMsg)
-    return None
